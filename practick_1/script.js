ec230549ef7c5709076530217630c0bd1aa2d23b
Denis <darksaude161190@yandex.ru>
2024-11-14 21:23:35 +0300

    Добавление данных пользователя

--- practick_1/script.js
+++ practick_1/script.js
@@ -1,1 +1,23 @@
 "use strics";
+
+// Переменная ответов
+
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", ""),
+    a = prompt("Один из последних просмотренных фильмов?"),
+    b = +prompt("На сколько оцените его?");
+
+const personalMovieDB = {
+    "counst": numberOfFilms,
+    "movies": {},
+    "actors": {},
+    "genres": [],
+    "privat": false
+};
+
+personalMovieDB.movies[a] = b;
+
+console.log(numberOfFilms);
+console.log(personalMovieDB);
+
+
+
